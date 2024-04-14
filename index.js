450d652d2ac87e0fa52200bd5f1d8400ba839b22
Aryan1982 <ap1982o3@gmail.com>
2024-04-14 10:59:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const socket = require("socket.io")
+const app = express();
+
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// static files
+app.use(express.static('public'));
+
+// Socket
+const io = socket(server);
+
+io.on('connect', function(socket){
+    console.log("connection made on socket", socket.id)
+
+    // socket.on('chat', function(data){
+    //    io.sockets.emit('chat',data);
+    // })
+
+    socket.on("join-room",(room)=>{
+        socket.join(room.roomId)
+        console.log(`User joined room: ${room.roomId}`);
+    })
+
+    socket.on("chat", (data) =>{
+        console.log(data)
+        io.to(data.room).emit('chat',data);
+    })
+})
+
---
+++ public/chat.js
@@ -0,0 +1,33 @@
+// Make connection
+
+const socket = io.connect("http://localhost:3000");
+
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    joinRoomBtn = document.getElementById("join"),
+    roomId = document.getElementById('room')
+// Emit events
+btn.addEventListener('click', function(event){
+    event.preventDefault();
+
+    socket.emit('chat', {
+        message: message.value,
+        room: roomId.value,
+        handle: handle.value,
+    });
+    message.value = "";
+});
+
+joinRoomBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    socket.emit('join-room',{
+        roomId: roomId.value
+    })
+    joinRoomBtn.innerHTML=  `joined ${roomId.value}`
+})
+// Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
